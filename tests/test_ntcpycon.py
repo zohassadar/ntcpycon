4258f581966e9fa1c975f144b086fd950d9865a7
Richard <zohassadar@gmail.com>
2022-08-23 20:47:52 -0700

    test importing everything (least we can do)

--- tests/test_ntcpycon.py
+++ tests/test_ntcpycon.py
@@ -1,5 +1,17 @@
 from ntcpycon import __version__
 
+import ntcpycon.abstract
+import ntcpycon.binaryframe
+import ntcpycon.config
+import ntcpycon.connect
+import ntcpycon.file_handler
+import ntcpycon.pcap_replay
+import ntcpycon.tcp_server
+import ntcpycon.ws_sender
+
 
 def test_version():
     assert __version__ == "0.1.0"
+
+
+
