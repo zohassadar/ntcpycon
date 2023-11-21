297908dc54d815f377e98d67ac7c23a157897b04
Richard <zohassadar@gmail.com>
2023-11-21 10:48:36 -0700

    formatting

--- ntcpycon/binaryframe.py
+++ ntcpycon/binaryframe.py
@@ -31,6 +31,7 @@ def decode_payload(payload: bytes) -> dict:
 class BinaryFrame3:
     VERSION = 3
     GAME_TYPE = 1
+
     def __init__(self):
         self.playfield = bytearray(200)
         self.t = 0
@@ -51,54 +52,58 @@ class BinaryFrame3:
         self.cur_piece = 0
         self.cur_piece_das = 0
         self.next_id = 0
-        
+
     @property
     def stats(self) -> bytearray:
         _stats = bytearray(9)
-        _stats[0] = ((self.t & 0b1111111100) >> 2)
+        _stats[0] = (self.t & 0b1111111100) >> 2
         _stats[1] = ((self.t & 0b0000000011) << 6) | ((self.j & 0b1111110000) >> 4)
         _stats[2] = ((self.j & 0b0000001111) << 4) | ((self.z & 0b1111000000) >> 6)
         _stats[3] = ((self.z & 0b0000111111) << 2) | ((self.o & 0b1100000000) >> 8)
-        _stats[4] = ((self.o & 0b0011111111) << 0)
-        _stats[5] = ((self.s & 0b1111111100) >> 2)
+        _stats[4] = (self.o & 0b0011111111) << 0
+        _stats[5] = (self.s & 0b1111111100) >> 2
         _stats[6] = ((self.s & 0b0000000011) << 6) | ((self.l & 0b1111110000) >> 4)
         _stats[7] = ((self.l & 0b0000001111) << 4) | ((self.i & 0b1111000000) >> 6)
-        _stats[8] = ((self.i & 0b0000111111) << 2)
+        _stats[8] = (self.i & 0b0000111111) << 2
         return _stats
-    
+
     @property
     def compressed(self) -> bytearray:
         _compressed = bytearray(50)
         for i in range(50):
-            _compressed[i] = \
-                (self.playfield[i*4]   & 3) << 6 | \
-                (self.playfield[i*4+1] & 3) << 4 | \
-                (self.playfield[i*4+2] & 3) << 2 | \
-                (self.playfield[i*4+3] & 3) 
+            _compressed[i] = (
+                (self.playfield[i * 4] & 3) << 6
+                | (self.playfield[i * 4 + 1] & 3) << 4
+                | (self.playfield[i * 4 + 2] & 3) << 2
+                | (self.playfield[i * 4 + 3] & 3)
+            )
+
     @property
     def payload(self):
         _payload = bytearray(73)
-        _payload[0] = ((self.FRAME_VERSION & 0b111) << 5) | ((self.GAME_TYPE & 0b11) << 3)
+        _payload[0] = ((self.FRAME_VERSION & 0b111) << 5) | (
+            (self.GAME_TYPE & 0b11) << 3
+        )
 
-        _payload[1] = (self.game_id & 0xff00) >> 8
-        _payload[2] = (self.game_id & 0x00ff) >> 0
+        _payload[1] = (self.game_id & 0xFF00) >> 8
+        _payload[2] = (self.game_id & 0x00FF) >> 0
 
         # ctime - 28 bits
-        _payload[3] = (self.elapsed & 0xff00000) >> 20
-        _payload[4] = (self.elapsed & 0x00ff000) >> 12
-        _payload[5] = (self.elapsed & 0x0000ff0) >> 4
-        _payload[6] = ((self.elapsed & 0x0f) << 4) | ((self.lines & 0xf00) >> 8)
+        _payload[3] = (self.elapsed & 0xFF00000) >> 20
+        _payload[4] = (self.elapsed & 0x00FF000) >> 12
+        _payload[5] = (self.elapsed & 0x0000FF0) >> 4
+        _payload[6] = ((self.elapsed & 0x0F) << 4) | ((self.lines & 0xF00) >> 8)
 
         # lines - 12 bits
-        _payload[7] = self.lines & 0xff
+        _payload[7] = self.lines & 0xFF
 
         # level - 8 bits
         _payload[8] = self.level
 
         # score - 24 bits
-        _payload[9] =  (self.score & 0xff0000) >> 16
-        _payload[10] = (self.score & 0x00ff00) >> 8
-        _payload[11] = self.score & 0x0000ff
+        _payload[9] = (self.score & 0xFF0000) >> 16
+        _payload[10] = (self.score & 0x00FF00) >> 8
+        _payload[11] = self.score & 0x0000FF
         _payload[12] = ((self.instant_das & 0b11111) << 3) | (self.next_id & 0b111)
         _payload[13] = ((self.cur_piece_das & 0b11111) << 3) | (self.cur_piece & 0b111)
         _payload[14:23] = self.stats
