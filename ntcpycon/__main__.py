38fcdcb54b4b931f7ff95460252deb286bb5e6e7
Richard <zohassadar@gmail.com>
2023-11-27 04:50:11 -0700

    remove temp entrypoint

--- ntcpycon/__main__.py
+++ ntcpycon/__main__.py
@@ -1,8 +1,2 @@
-import sys
-if len(sys.argv) == 1:
-    import ntcpycon.connect
-    ntcpycon.connect.start_connect()
-elif sys.argv[1] == "edlink-test":
-    # temp entrypoint to test
-    import ntcpycon.edlink
-    ntcpycon.edlink.edlink()
+import ntcpycon.connect
+ntcpycon.connect.start_connect()
