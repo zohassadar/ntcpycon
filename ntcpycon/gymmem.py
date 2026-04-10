20fb218a3e6c938f93322c5d935f5673903a67ec
zohassadar <zohassadar@gmail.com>
2026-04-10 16:18:27 -0700

    use consecutive values

--- ntcpycon/gymmem.py
+++ ntcpycon/gymmem.py
@@ -315,7 +315,7 @@ class GymMemory:
         if self._topout and not self._topout_timer:
             return 0x3fe
         if self.gamemode != 4:
-            return 0x3ef
+            return 0x3ff
         return self._hybrid_bcd_convert(hi, lo)
 
     @property
